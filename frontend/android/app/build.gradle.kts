989e3d9b02e0bcebf313559db2259150baa85774
dizzpy <dizzzpy@gmail.com>
2026-04-05 15:33:27 +0530

    chore(app): app icon, package name, app name changed

--- frontend/android/app/build.gradle.kts
+++ frontend/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "dev.dizzpy.swaptune"
+    namespace = "dev.dizzpy.swaptunes"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "dev.dizzpy.swaptune"
+        applicationId = "dev.dizzpy.swaptunes"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
